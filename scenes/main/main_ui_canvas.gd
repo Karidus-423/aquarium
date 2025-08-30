4d368bce5d47fb99fc59c48636009bb55e03faba
Karidus-423 <kapuerto23@gmail.com>
2025-08-29 23:54:26 -0400

    locking plz

---
+++ scenes/main/main_ui_canvas.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+## Get all children buttons in the Hbox container.
+
+## When focus is on one of the children. Change the
+## fov of the camaera that is used on the viewport
+## for the button. Which would be an animation.
